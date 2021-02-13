eefffb3dd7f7219fb3b07a725bbfcd48c1b0cab2
Johann Alberts <alberts.johann@gmail.com>
2021-02-13 21:21:44 +0000

    Add 'name' parameter and dictionary of names

--- app.py
+++ app.py
@@ -4,13 +4,26 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+names = {
+    "johann":
+        {
+            "nationality": "South African",
+            "age": 38
+        },
+    "carlos":
+        {
+            "nationality": "Spanish",
+            "age": 39
+        }
+}
+
 
 class HelloWorld(Resource):
-    def get(self):
-        return {"data": "Hello World"}
+    def get(self, name):
+        return names[name]
 
 
-api.add_resource(HelloWorld, "/helloworld")
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
 
 if __name__ == "__main__":
     app.run(debug=True)
