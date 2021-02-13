7b64095f5061a8cd8425847141afe487bf302278
Johann Alberts <alberts.johann@gmail.com>
2021-02-13 22:52:03 +0000

    Add video class, arg parser and abort function

--- app.py
+++ app.py
@@ -1,29 +1,37 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
-names = {
-    "johann":
-        {
-            "nationality": "South African",
-            "age": 38
-        },
-    "carlos":
-        {
-            "nationality": "Spanish",
-            "age": 39
-        }
-}
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument(
+    "name", type=str, help="Video name required", required=True)
+video_put_args.add_argument(
+    "views", type=int, help="Number of views required", required=True)
+video_put_args.add_argument(
+    "likes", type=int, help="Number of likes required", required=True)
+
+videos = {}
 
 
-class HelloWorld(Resource):
-    def get(self, name):
-        return names[name]
+def abort_if_video_id_doesnt_exist(video_id):
+    if video_id not in videos:
+        abort(404, message="Invalid video ID!")
 
 
-api.add_resource(HelloWorld, "/helloworld/<string:name>")
+class Video(Resource):
+    def get(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
+        return videos[video_id]
+
+    def put(self, video_id):
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201
+
+
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,5 +2,9 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "helloworld/johann")
+response = requests.put(
+    BASE + "video/1", {"name": "First video", "views": 5, "likes": 10})
+print(response.json())
+input()
+response = requests.get(BASE + "video/2")
 print(response.json())
